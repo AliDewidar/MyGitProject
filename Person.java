2aeec9a89b503b9dfe9a6924c46449a7b067dae5
AliDewidar <alidewidar23718@gmail.com>
2024-10-19 17:19:05 +0300

    Modified the Person file

--- Person.java
+++ Person.java
@@ -4,6 +4,11 @@
 public class Person {
 
     public static void main(String[] args) {
+        // System.out.println("Hello alwa from github");
+        Person p = new Person();
+        p.sayHello();
+    }
+    public void sayHello(){
         System.out.println("Hello alwa from github");
     }
 }
